1c0523bbac350888d9fa9c5817a569d749c56fdc
Basant Bhardwaj <basantibhardwaj8@gmail.com>
2025-04-09 15:46:09 +0530

    updatesss

--- backend/server.js
+++ backend/server.js
@@ -11,19 +11,10 @@ const app = express();
 const allowedOrigins = ['https://todo-app-ao23.vercel.app'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    // allow requests with no origin like mobile apps or curl
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
-
 // Handle preflight requests (important for some browsers)
 app.options('*', cors());
 
